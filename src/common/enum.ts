fcfc32c1730270e13e09f6ef8f8a9f4d7be8bf6d
astaniano <debryniuk@gmail.com>
2024-01-04 00:35:34 +0200

    added NODE_ENV env var

---
+++ src/common/enum.ts
@@ -0,0 +1,4 @@
+export const ENV = {
+    PROD: 'prod',
+    DEV: 'dev',
+}
--- src/common/modules/config/config.service.ts
+++ src/common/modules/config/config.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ENV } from 'src/common/enum';
 
 @Injectable()
 export class ConfigService {
   public dbUrl: string;
+  public currentEnv: string;
 
   constructor() {
     this.dbUrl = this.loadEnv('DATABASE_URL');
+    this.currentEnv = this.loadEnvIfInEnum('NODE_ENV', Object.values(ENV));
   }
 
   private loadEnv(key: string) {
@@ -13,6 +16,17 @@ export class ConfigService {
 
     if (!envVar) {
       throw new Error(`${key} is a required env variable`);
+    }
+
+    return envVar;
+  }
+
+  private loadEnvIfInEnum(key: string, enumValues: string[]) {
+    const envVar = process.env[key];
+    
+    const envVarIsOneOfEnumValues = enumValues.find((val) => val === envVar)
+    if (!envVarIsOneOfEnumValues) {
+      throw new Error(`${key} env var must be one of the following: ${enumValues}`);
     }
 
     return envVar;
